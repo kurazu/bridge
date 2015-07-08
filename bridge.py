20a212a7cf90b8ce7d004eecaa90ff0ac62690db
Tomasz Maćkowiak <kurazu@kurazu.net>
2015-07-08 14:56:43 +0200

    trying to get v8 to work

--- bridge.py
+++ bridge.py
@@ -1,3 +1,17 @@
 import runjs
 
 print(runjs.run("2 + 3"))
+
+js = None
+
+
+def funca(a, b):
+    """add"""
+    return js.execute('return a + b', [a, b])
+
+
+@js
+def funcb(a, b):
+    """
+    return a + b;
+    """
--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from distutils.core import setup, Extension
 
 runjs = Extension(
     'runjs',
-    sources=['runjs.c'],
-    #include_dirs=['/usr/local/include'],
-    #libraries=['tcl83'],
-    #library_dirs=['/usr/local/lib']
+    sources=['runjs.cpp'],
+    include_dirs=['/usr/local/include', '/home/kurazu/apps/v8'],
+    #libraries=['v8', 'icui18n', 'icuuc'],
+    #library_dirs=[
+    #    '/usr/local/lib', '/home/kurazu/apps/v8/out/native/lib.target'
+    #],
+    language='c++',
+    extra_compile_args=['-std=c++0x'],
+    extra_link_args='-Wl,--start-group /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_base.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libbase.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libplatform.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicuuc.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicui18n.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicudata.a -Wl,--end-group -lrt -ldl'.split(' ')
 )
 
 
