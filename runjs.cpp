f5f86ac306968c92afa905b22b96f5352f1d047b
Tomasz Maćkowiak <kurazu@kurazu.net>
2015-07-08 15:56:28 +0200

    use boost

--- runjs.cpp
+++ runjs.cpp
@@ -1,79 +1,57 @@
-#include <Python.h>
-#include "include/v8.h"
-#include "include/libplatform/libplatform.h"
+#include <iostream>
+// #include "include/v8.h"
+// #include "include/libplatform/libplatform.h"
 
-using namespace v8;
+// using namespace v8;
 
+// void run_in_v8(const char* code) {
+//   V8::InitializeICU();
+//   Platform* platform = platform::CreateDefaultPlatform();
+//   V8::InitializePlatform(platform);
+//   V8::Initialize();
+
+//   // Create a new Isolate and make it the current one.
+//   Isolate* isolate = Isolate::New();
+//   {
+//     Isolate::Scope isolate_scope(isolate);
+
+//     // Create a stack-allocated handle scope.
+//     HandleScope handle_scope(isolate);
+
+//     // Create a new context.
+//     Local<Context> context = Context::New(isolate);
+
+//     // Enter the context for compiling and running the hello world script.
+//     Context::Scope context_scope(context);
+
+//     // Create a string containing the JavaScript source code.
+//     Local<String> source = String::NewFromUtf8(isolate, code);
+
+//     // Compile the source code.
+//     Local<Script> script = Script::Compile(source);
+
+//     // Run the script to get the result.
+//     Local<Value> result = script->Run();
+
+//     // Convert the result to an UTF8 string and print it.
+//     String::Utf8Value utf8(result);
+//     std::cout << "Result: " << *utf8 << "\n";
+//   }
+
+//   // Dispose the isolate and tear down V8.
+//   isolate->Dispose();
+//   V8::Dispose();
+//   V8::ShutdownPlatform();
+//   delete platform;
+// }
 void run_in_v8(const char* code) {
-  V8::InitializeICU();
-  Platform* platform = platform::CreateDefaultPlatform();
-  V8::InitializePlatform(platform);
-  V8::Initialize();
-
-  // Create a new Isolate and make it the current one.
-  Isolate* isolate = Isolate::New();
-  {
-    Isolate::Scope isolate_scope(isolate);
-
-    // Create a stack-allocated handle scope.
-    HandleScope handle_scope(isolate);
-
-    // Create a new context.
-    Local<Context> context = Context::New(isolate);
-
-    // Enter the context for compiling and running the hello world script.
-    Context::Scope context_scope(context);
-
-    // Create a string containing the JavaScript source code.
-    Local<String> source = String::NewFromUtf8(isolate, code);
-
-    // Compile the source code.
-    Local<Script> script = Script::Compile(source);
-
-    // Run the script to get the result.
-    Local<Value> result = script->Run();
-
-    // Convert the result to an UTF8 string and print it.
-    String::Utf8Value utf8(result);
-    printf("RESULT: %s\n", *utf8);
-  }
-
-  // Dispose the isolate and tear down V8.
-  isolate->Dispose();
-  V8::Dispose();
-  V8::ShutdownPlatform();
-  delete platform;
+    std::cout << "Result: " << code << "\n";
 }
 
-extern "C" {
+#include <boost/python.hpp>
+using namespace boost::python;
 
-    static PyObject *
-    runjs_run(PyObject *self, PyObject *args) {
-        const char *code;
-
-        if (!PyArg_ParseTuple(args, "s", &code))
-            return NULL;
-        run_in_v8(code);
-        return PyLong_FromLong(5);
-    }
-
-    static PyMethodDef RunjsMethods[] = {
-        {"run",  runjs_run, METH_VARARGS, "Execute JavaScript."},
-        {NULL, NULL, 0, NULL} /* Sentinel */
-    };
-
-    static struct PyModuleDef runjsmodule = {
-       PyModuleDef_HEAD_INIT,
-       "runjs", /* name of module */
-       "Module for executing JavaScript", /* module documentation, may be NULL */
-       -1, /* size of per-interpreter state of the module,
-              or -1 if the module keeps state in global variables. */
-       RunjsMethods
-    };
-
-    PyMODINIT_FUNC
-    PyInit_runjs(void) {
-        return PyModule_Create(&runjsmodule);
-    }
-
+BOOST_PYTHON_MODULE(runjs)
+{
+    def("run", run_in_v8);
 }
--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from distutils.core import setup, Extension
 runjs = Extension(
     'runjs',
     sources=['runjs.cpp'],
-    include_dirs=['/usr/local/include', '/home/kurazu/apps/v8'],
-    #libraries=['v8', 'icui18n', 'icuuc'],
-    #library_dirs=[
-    #    '/usr/local/lib', '/home/kurazu/apps/v8/out/native/lib.target'
-    #],
-    language='c++',
+    include_dirs=['/usr/local/include', '/home/kurazu/apps/v8', '/home/kurazu/apps/boost/include'],
+    libraries=['boost_python', 'v8', 'icui18n', 'icuuc'],
+    library_dirs=[
+        '/usr/local/lib', '/home/kurazu/apps/v8/out/native/lib.target', '/home/kurazu/apps/boost/lib'
+    ],
     extra_compile_args=['-std=c++0x'],
-    extra_link_args='-Wl,--start-group /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_base.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libbase.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libplatform.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicuuc.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicui18n.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicudata.a -Wl,--end-group -lrt -ldl'.split(' ')
+    #extra_link_args='-Wl,--start-group /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_base.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libbase.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libplatform.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicuuc.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicui18n.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicudata.a -Wl,--end-group -lrt -ldl'.split(' ')
 )
 
 
