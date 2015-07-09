075b129546c3859ecb45da54385aa90b6ced5a77
Tomasz Maćkowiak <kurazu@kurazu.net>
2015-07-09 12:06:56 +0200

    fighting with compilation

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ runjs = Extension(
     include_dirs=['/usr/local/include', '/home/kurazu/apps/v8', '/home/kurazu/apps/boost/include'],
     libraries=['boost_python', 'v8', 'icui18n', 'icuuc'],
     library_dirs=[
-        '/usr/local/lib', '/home/kurazu/apps/v8/out/native/lib.target', '/home/kurazu/apps/boost/lib'
+        '/usr/local/lib', '/home/kurazu/apps/v8/out/x64.release/lib.target', '/home/kurazu/apps/boost/lib'
     ],
     extra_compile_args=['-std=c++0x'],
+    extra_link_args=['-undefined dynamic_lookup'],
     #extra_link_args='-Wl,--start-group /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_base.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libbase.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a /home/kurazu/apps/v8/out/native/obj.target/tools/gyp/libv8_libplatform.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicuuc.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicui18n.a /home/kurazu/apps/v8/out/native/obj.target/third_party/icu/libicudata.a -Wl,--end-group -lrt -ldl'.split(' ')
 )
 
